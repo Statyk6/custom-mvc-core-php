2a34c244c526ebd8f27019c06ae719befee5ef5e
Alexandr Kravets <50145254+Statyk6@users.noreply.github.com>
2023-12-12 10:17:54 +0300

    Add files via upload

---
+++ application/core/Router.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace application\core;
+
+class Router {
+    
+    protected $routes = [];
+    protected $params = [];
+
+    public function __construct() {
+        //
+    }
+
+    public function add() { // функция добавления маршрута
+        //
+    }
+
+    public function match() { // функция проверки маршрута
+        //
+    }
+    
+    public function run() { // функция которая запускает роутер
+        echo 'Старт';
+    }
+}
